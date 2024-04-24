43f596980758dcb32fde91ebd94747a1b91c5a0b
AnnikDev <a_kvernadze@cu.edu.ge>
2024-04-24 15:36:56 +0400

    delete useless files

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import "./index.css";
+import styled from "styled-components";
+import InputContainer from "./components/InputContainer/Input";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Splitter>SPLITTER</Splitter>
+      <InputContainer/>
     </>
-  )
+  );
 }
 
-export default App
+const Splitter = styled.h1`
+  font-size: 1.6rem;
+  letter-spacing: 0.2cm;
+  color: #3d6666;
+  word-wrap: break-word;
+  width: 8.6rem;
+  margin: 16.3rem auto 8.7rem;
+`;
+
+export default App;
---
+++ src/components/InputContainer/Input.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export default function InputContainer() {
+  return <Container />;
+  
+}
+
+const Container = styled.div`
+  width: 92rem;
+  height: 48.1rem;
+  padding: 3.2rem 3.2rem 3.2rem 4.8rem;
+  background-color: #fff;
+  border-radius: 2.5rem;
+`;
